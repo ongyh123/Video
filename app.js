031e57403ac6ccac1770a9a7c4f84a458683e64d
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2023-01-08 19:39:30 +0800

    add Event Listener

--- app.js
+++ app.js
@@ -5,3 +5,14 @@
 // Get the DOM element
 const btn = document.querySelector('.switch-btn');
 const video = document.querySelector('.video-container');
+
+// Listen to the Click Event
+btn.addEventListener('click', function () {
+  if (!btn.classList.contains('slide')) {
+    btn.classList.add('slide');
+    video.pause();
+  } else {
+    btn.classList.remove('slide');
+    video.play();
+  }
+});
